cb35fe87cfd64cfc6d083c9839f0ca1a31813e82
Vtornikov Evgeny <phtor@mail.ru>
2022-04-05 11:06:58 +0300

    lesson 04

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
+
+
 let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?');
 let screenPrice = +prompt('Сколько будет стоить данная работа?');
 let adaptive = confirm('Нужен ли адаптив на сайте?');
+
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
+
 let rollback = 10;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2; 
-let servicePercentPrice = fullPrice - (fullPrice * (rollback/100));
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
-if(fullPrice > 30000){	
-	console.log("Даем скидку в 10%");
-}
-else if(fullPrice <= 30000 && fullPrice > 15000){
-	console.log("Даем скидку в 5%");
-}
-else if(fullPrice >= 0 && fullPrice <= 15000){
-	console.log("Скидка не предусмотрена");
-}
-else{
-	console.log("Что то пошло не так");
+const getAllServicePrices = () =>{
+	return servicePrice1 + servicePrice2;	
 }
 
-console.log(typeof title);
-console.log(typeof screenPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
+const showTypeOf = function (variable){
+	console.log(variable, typeof variable);
+}
+
+const getFullPrice = function(){
+	return screenPrice + allServicePrices;
+}
+
+const getServicePercentPrices = function(){
+	return fullPrice - (fullPrice * (rollback / 100))
+}
+
+const getTitle = () =>{
+	let splitted = title.trim().toLowerCase().split("");
+	const first = splitted[0].toUpperCase();	
+	splitted.splice(0, 1);	
+	const result = [ ...first, ...splitted];	
+	return title = result.join("");	
+}
+
+const getRollbackMessage = (price) =>{
+	if(price > 30000){	
+		return "Даем скидку в 10%";
+	}else if(price <= 30000 && price > 15000){
+		return "Даем скидку в 5%";
+	}else if(price >= 0 && price <= 15000){
+		return "Скидка не предусмотрена";
+	}else{
+		return "Что то пошло не так";
+	}
+}
+
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(fullPrice);
 console.log(servicePercentPrice);
-console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юаней");
